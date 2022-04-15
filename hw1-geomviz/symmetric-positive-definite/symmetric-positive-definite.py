e4cb50a5b62388808da931cb9de81fa52f515204
taco650 <taco650@gmail.com>
2022-04-15 11:01:08 -0700

    init

---
+++ hw1-geomviz/symmetric-positive-definite/symmetric-positive-definite.py
@@ -0,0 +1,20 @@
+def plot():
+    pass
+
+def plot_grid():
+    pass
+
+def plot_rendering():
+    pass
+
+def plot_tangent_space():
+    pass
+
+def scatter():
+    pass
+
+def plot_geodesic():
+    pass
+
+def plot_vector_field():
+    pass
