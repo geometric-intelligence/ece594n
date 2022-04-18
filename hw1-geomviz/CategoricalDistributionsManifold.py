0afea8e6b2cc92c92158dd003b5da0336be4c293
siyu lin <siyulin@umail.ucsb.edu>
2022-04-17 18:19:09 -0700

    Add visualization for geodesic and random points for 2D and 3D manifold

--- hw1-geomviz/CategoricalDistributionsManifold.py
+++ hw1-geomviz/CategoricalDistributionsManifold.py
@@ -2,16 +2,18 @@ from cmath import nan
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
 import numpy as np
+from geomstats.information_geometry.categorical import CategoricalDistributions, CategoricalMetric
 class CategoricalDistributionsManifold:
     def __init__(self, dim):
         self.dim = dim
         self.points = []
         self.ax = None
         self.elev, self.azim = None, None
-    
+        self.metric = CategoricalMetric(dim = self.dim)
     def plot(self):
         min_limit = 0
         max_limit = 1
+        plt.figure(dpi = 100)
         self.set_axis(min_limit, max_limit)
         if self.dim == 3:
             self.set_view()
@@ -27,13 +29,18 @@ class CategoricalDistributionsManifold:
             vertices = [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]]
             tupleList = list(zip(x, y, z))
             poly3d = [[tupleList[vertices[ix][iy]] for iy in range(len(vertices[0]))] for ix in range(len(vertices))]
-            self.ax.add_collection3d(Poly3DCollection(poly3d, edgecolors='k', facecolors='w', linewidths=3, alpha=0.5))
+            self.ax.add_collection3d(Poly3DCollection(poly3d, edgecolors='k', facecolors='w', linewidths=3, alpha=0.2))
 
+            for point in self.points:
+                self.ax.scatter(point[0], point[1], point[2])
 
         elif self.dim == 2:
             X = np.linspace(start = min_limit, stop = max_limit, num = 101, endpoint = True)
             Y = 1 - X
-            self.ax.fill_between(X, Y)
+            # self.ax.fill_between(X, Y)
+            self.ax.plot(X, Y)
+            for point in self.points:
+                self.ax.scatter(point[0], point[1])
 
 
     def set_view(self, elev = 30.0, azim = 20.0):
@@ -42,6 +49,28 @@ class CategoricalDistributionsManifold:
                 self.set_axis()
             self.elev, self.azim = elev, azim
             self.ax.view_init(elev, azim)
+
+    def set_points(self, points):
+        self.points = points
+
+    def plot_geodesic(self, initial_point, end_point = None, tangent_vector = None):
+        self.plot()
+        geodesic = self.metric.geodesic(initial_point=initial_point, end_point = end_point, initial_tangent_vec = tangent_vector)
+        num_samples = 100
+        if self.dim == 3:
+            for i in range(num_samples):
+                point = geodesic(i/num_samples)
+                self.ax.scatter(point[0], point[1], point[2], color='blue')
+            self.ax.scatter(geodesic(0)[0], geodesic(0)[1], geodesic(0)[2], color='red', s = 30)
+        elif self.dim == 2:
+            for i in range(num_samples):
+                point = geodesic(i/num_samples)
+                self.ax.scatter(point[0], point[1], color='blue', s = 5)
+            self.ax.scatter(geodesic(0)[0], geodesic(0)[1], color='red', s = 30)
+
+
+    def clear_points(self):
+        self.points = []
 
     def set_axis(self, min_limit, max_limit):
         if self.dim == 3: 
@@ -68,5 +97,3 @@ class CategoricalDistributionsManifold:
                 aspect = "equal")
 
         self.ax = ax
-
-
