e85d9fcd03448b297b9452d12bd7ee05bee3cf98
taco650 <taco650@gmail.com>
2022-04-21 09:22:51 -0700

    wip

--- hw1-geomviz/symmetric_positive_definite/symmetric_positive_definite.py
+++ hw1-geomviz/symmetric_positive_definite/symmetric_positive_definite.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import colorsys
 import math
-from matplotlib.patches import Ellipse
 import mpl_toolkits.mplot3d.art3d as art3d
 from geomstats.visualization import Ellipses
 from matplotlib.tri import Triangulation
@@ -14,11 +13,13 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
 
 class SymmetricPositiveDefiniteVizualization:
+    
     def __init__(self, maxZ = 1):
         self.maxZ = float(maxZ)
         self.currZ = self.maxZ
         self.ax = None
-        self.spdSpace = Ellipses()
+        self.spdPointViz = Ellipses()
+        self.spdManifold = SPDMatrices(2)
 
     def cuboid_data(self, o, size=(1,1,1)):
         X = [[[0, 1, 0], [0, 0, 0], [1, 0, 0], [1, 1, 0]],
@@ -44,47 +45,28 @@ class SymmetricPositiveDefiniteVizualization:
         return Poly3DCollection(np.concatenate(g),  
                                 facecolors=np.repeat(colors,6, axis=0), **kwargs)
 
-    def animate(self, frame_num):
-        # rotate the axes and update
-        self.ax.view_init(25, frame_num*10)
-        # plt.draw()
-        # return plt
-        return self.ax
 
-
-    def plot(self, currZ=None, hsv=False):
+    def plot(self,  n_angles = 80, n_radii = 40, currZ=None, hsv=False):
+        
         if currZ == None:
             self.currZ = self.maxZ
         else:
             self.currZ = currZ
 
         #Modified from: https://stackoverflow.com/questions/55298164/3d-plot-of-the-cone-using-matplotlib
-        n_angles = 80
-
-        n_radii = 40
-
-
         # An array of radii
-
         # Does not include radius r=0, this is to eliminate duplicate points
         radii = np.linspace(0.0, self.currZ, n_radii)
-
 
         # An array of angles
         angles = np.linspace(0, 2*np.pi, n_angles, endpoint=False)
 
-
         # Repeat all angles for each radius
         angles = np.repeat(angles[..., np.newaxis], n_radii, axis=1)
 
-
         # Convert polar (radii, angles) coords to cartesian (x, y) coords
-
         # (0, 0) is added here. There are no duplicate points in the (x, y) plane
-
-
         x = np.append(0, (radii*np.cos(angles)).flatten())
-
         y = np.append(0, (radii*np.sin(angles)).flatten())
 
 
@@ -92,25 +74,18 @@ class SymmetricPositiveDefiniteVizualization:
         z = np.full_like(x, self.currZ)
 
         # print(x.shape, y.shape, angles.shape, radii.shape, z.shape)
-
         # # NOTE: This assumes that there is a nice projection of the surface into the x/y-plane!
         tri = Triangulation(x, y)
-
         triangle_vertices = np.array([np.array([[x[T[0]], y[T[0]], z[T[0]]],
 
                                                 [x[T[1]], y[T[1]], z[T[1]]],
 
                                                 [x[T[2]], y[T[2]], z[T[2]]]]) for T in tri.triangles])
-
-
         x2 = np.append(0, (radii*np.cos(angles)).flatten())
-
         y2 = np.append(0, (radii*np.sin(angles)).flatten())
-
-
+        
         # Pringle surface
         z2 = np.sqrt(x**2+y**2)
-
 
         # NOTE: This assumes that there is a nice projection of the surface into the x/y-plane!
         tri2 = Triangulation(x2, y2)
@@ -122,43 +97,28 @@ class SymmetricPositiveDefiniteVizualization:
                                                 [x2[T[2]], y2[T[2]], z2[T[2]]]]) for T in tri2.triangles])
 
         triangle_vertices = np.concatenate([triangle_vertices, triangle_vertices2])
-
         midpoints = np.average(triangle_vertices, axis=1)
+        
         if hsv:
             facecolors = [self.find_color_for_point(pt) for pt in midpoints]  # smooth gradient
         else:
             facecolors = "0.75"  # grey
 
-
-        # hsv
         coll = Poly3DCollection(
             triangle_vertices, facecolors=facecolors, edgecolors=None,  alpha=0.7, zorder=-10)
-
+        self.artist = coll
         self.fig = plt.figure()
-
-         
+   
         self.ax = self.fig.gca(projection='3d')
-
-
-    
-        
         self.ax.add_collection(coll)
 
-
-        # self.ax.set_xlim(-self.currZ*1.25, self.currZ*1.25)
-
-        # self.ax.set_ylim(-self.currZ*1.25, self.currZ*1.25)
-
-        # self.ax.set_zlim(-self.currZ*.25, self.currZ*1.25)
-
-
         self.ax.set_xlim(-self.maxZ*1.25, self.maxZ*1.25)
-
         self.ax.set_ylim(-self.maxZ*1.25, self.maxZ*1.25)
-
         self.ax.set_zlim(-self.maxZ*.25, self.maxZ*1.25)
-
-        self.ax.elev = 90
+        self.ax.set_xlabel("X")
+        self.ax.set_ylabel("Y")
+        self.ax.set_zlabel("Z")
+        self.ax.elev = 26
         
 
     @staticmethod
@@ -168,10 +128,21 @@ class SymmetricPositiveDefiniteVizualization:
         #let x = b
         #let y = c
         
-        # a+b c
-        # c a-b
+        # [z+x, y]
+        # [y, z-x]
+        #z>0
+        #z^2 > x^2 + y^2
         
         return np.array([[z+x, y],[y, z-x]])
+    
+    @staticmethod
+    def spd_to_xyz(point):
+        z = (point[0,0] + point[1,1])/2.0
+        x = point[0,0]-z
+        y = point[0,1]
+        
+        
+        return (x,y,z)
 
 
     def find_color_for_point(self, point):
@@ -212,31 +183,26 @@ class SymmetricPositiveDefiniteVizualization:
 
         y = np.append(0, (radii*np.sin(angles)).flatten())
 
-        print("X Shape {}, Y Shape {}".format(x.shape, y.shape))
-
+        # print("X Shape {}, Y Shape {}".format(x.shape, y.shape))
         for x_tmp, y_tmp in zip(x,y):
-            print("X: {} Y:{}".format(x_tmp, y_tmp))
+            # print("X: {} Y:{}".format(x_tmp, y_tmp))
             sampled_xyz = (x_tmp, y_tmp, z_plane)
             sampled_spd = SymmetricPositiveDefiniteVizualization.xyz_to_spd(sampled_xyz)
-            print(sampled_spd)
-            ellipse_x, ellipse_y = self.spdSpace.compute_coordinates(sampled_spd)
+            # print(sampled_spd)
+            ellipse_x, ellipse_y = self.spdPointViz.compute_coordinates(sampled_spd)
             ellipse_z = np.full_like(ellipse_x, z_plane)
             self.ax.plot(ellipse_x/(n_radii*n_angles*0.25)+sampled_xyz[0], ellipse_y/(n_radii*n_angles*0.25)+sampled_xyz[1], sampled_xyz[2], alpha = 0.8, zorder=10, color = self.find_color_for_point(sampled_xyz))
 
 
-    def plot_rendering_side(self):
-        pass
-
-    def plot_rendering(self, point):
+    def plot_rendering(self, n_radii=5, n_angles=16):
         '''
         draws the manifold with regularly sampled data.
         '''
+        
+        self.ax.elev = 90
 
         #plot top surface points
-        self.plot_rendering_top(5, 16)
-
-        #plot side points
-        self.plot_rendering_side()
+        self.plot_rendering_top(n_radii, n_angles)
 
         
         
@@ -247,31 +213,68 @@ class SymmetricPositiveDefiniteVizualization:
         positions = np.array([[x, y,  z]])
         pc = self.plotCubeAt(positions, sizes=[(.1,.1,.1*.5)]*len(positions), edgecolor="k",  alpha=0.8, zorder=10)
         self.ax.add_collection3d(pc)
-        # ## ax.add_collection(pc)
+ 
         
 
     def scatter():
         pass
 
-    def plot_geodesic():
-        pass
+    def plot_geodesic(self, startPoint, endPoint):
+        """
+        allows to visualise a (discretised) geodesic. Takes either point and tangent vec as parameters, or initial point and end point as parameters.
+        """
+       
+        # sphere = Hypersphere(dim=2)
+        # point = np.array([-0.65726771, -0.02678122, 0.7531812])
+        # vector = np.array([1, 0, 0.8])
+        # tangent_vector = sphere.to_tangent(vector, base_point=point)
+        # result = sphere.metric.exp(tangent_vector, base_point=point)
+        baseMatrix = np.array([[1,0],[0,1]])
+        point = self.spdManifold.projection(baseMatrix)
+        print(point)
+
+        finalMatrix = np.array([[1+0.5, 0.5],[0.5, 1-0.5]])
+        # vector = self.spdManifold.to_vector(finalMatrix)
+        tangent_vector = self.spdManifold.random_tangent_vec(base_point=point)
+        print(tangent_vector)
+        # point = np.array([0,0,1])
+        # vector = np.array([0.5,0.5,0])
+        # tangent_vector = self.spdManifold.to_tangent(vector, base_point=point)
+        result = self.spdManifold.metric.exp(tangent_vector, base_point=point)
+
+        vectorBasePoint = self.spdManifold.to_vector(point)
+        print(vectorBasePoint)
+        plt.plot(vectorBasePoint, ax=self.ax, label="Point")
+        arrow = plt.Arrow3D(point, vector=tangent_vector) 
+        arrow.draw(self.ax, color="C0", label="Tangent Vector")
+        plt.plot(result, ax=self.ax, s=100, alpha=0.8, label="Exp", color="black")
+        
+
 
     def plot_vector_field():
         pass
 
 if __name__=="__main__":
+
+    from geomstats.geometry.hypersphere import Hypersphere
+
+
     import matplotlib.pyplot as plt
 
     import symmetric_positive_definite
     import geomstats.visualization as visualization
     from geomstats.geometry.spd_matrices import *
 
-    spdManifold = SPDMatrices(2)
+    from geomstats.geometry.euclidean import Euclidean, EuclideanMetric
+    # Affine-invariant Riemannian metric 
+
+    
     # randomPoints = spdManifold.random_point(n_samples=25000)
 
     # plot()
 
     # ellipses = visualization.Ellipses()
+
 
     # ellipses.draw_points(points=randomPoints)
     # symmetric_positive_definite.plot5()
@@ -281,19 +284,44 @@ if __name__=="__main__":
     viz = SymmetricPositiveDefiniteVizualization(1)
 
     viz.plot()
-    viz.plot_rendering(None)
-    plt.show(block=False)
+    viz.plot_geodesic(0,0)
+    plt.show()
+    # viz.plot(hsv=True)
+    # plt.show(block=False)
+    # input("Press Enter to continue...")
 
-    input("Press Enter to continue...")
+    # viz.plot(hsv=False)
+    # viz.plot_rendering()
+    # plt.show(block=False)
+    # input("Press Enter to continue...")
+
+    # viz.plot(currZ=0.9, hsv=False)
+    # viz.plot_rendering()
+    # plt.show(block=False)
+    # input("Press Enter to continue...")
+
+    # viz.plot(currZ=0.8, hsv=False)
+    # viz.plot_rendering()
+    # plt.show(block=False)
+    # input("Press Enter to continue...")
+
+    # viz.plot(currZ=0.7, hsv=False)
+    # viz.plot_rendering()
+    # plt.show(block=False)
+    # input("Press Enter to continue...")
+
+
+    # viz.plot(currZ=1, hsv=False)
+    # viz.plot_tangent_space((0,0,1))
+    # plt.show(block=False)
+    # input("Press Enter to continue...")
     
-    viz.plot(currZ=0.5)
-    viz.plot_rendering(None)
-    plt.show(block=False)
-    input("Press Enter to exit...")
+    # viz.plot(currZ=0.5)
+    # viz.plot_rendering(None)
+    # plt.show(block=False)
+    # input("Press Enter to exit...")
     # viz.plot_tangent_space((0,0,1))
     
-
-    # anim = FuncAnimation(viz.fig, viz.animate, frames=36, interval=2.5)
 
     # viz.plot_tangent_space((0.5,0.5,1))
     # viz.
