3cc3749db557bd3cbaffa2becf8279a88178c836
taco650 <taco650@gmail.com>
2022-06-01 13:54:15 -0700

    wip

---
+++ hw3-reviews/poincare-embeddings/poincare-embeddings.py
@@ -0,0 +1,6 @@
+# https://github.com/facebookresearch/poincare-embeddings
+
+# https://github.com/geomstats/geomstats/blob/a8f1d947608f1e116509f74da69b2f012de83e21/examples/learning_graph_embedding_and_predicting.py
+# https://github.com/geomstats/geomstats/blob/112dd8a29cd5712cbbd3627c976a9a4d77bd629d/geomstats/geometry/poincare_ball.py
+
+def :
