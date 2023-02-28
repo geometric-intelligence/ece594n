5a70cb677dd1a5279e086ca2568ca0409d5cbfd6
shaunchenucsb <shaunchen@ucsb.edu>
2023-02-28 00:22:38 -0800

    visualize schizo dim reduct

--- hw_shapes/schizo_dim_reduction/schizo_dim_reduction.py
+++ hw_shapes/schizo_dim_reduction/schizo_dim_reduction.py
@@ -1,2 +1,9 @@
 #Title: Visualization of the Brain Structure in Schizophrenia using Dimension Reduction Analysis
 #By: Shaun Chen
+
+import pandas
+df = pandas.read_csv('book-schizo.csv').to_numpy() #read csv file and convert to numpy array
+# print(df['x'][0])
+# print(df.loc[0])
+# print(type(df))
+print(df[0][1])
